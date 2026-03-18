de4f14ce8bf79b00f6e4d8432e491c8c9df72461
MdSaifu7 <mdsaifkhanahmad@gmail.com>
2026-03-19 01:21:22 +0530

    update axios base url

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,7 +4,12 @@ import authRoutes from "./router/auth.routes.js";
 import cors from "cors";
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/auth", authRoutes);
 app.use("/gadgets", gadgetRoutes);
 export default app;
--- front-end/src/api/AxiosConfig.jsx
+++ front-end/src/api/AxiosConfig.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
+
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://products-website-io.onrender.com",
 });
 
 export default instance;

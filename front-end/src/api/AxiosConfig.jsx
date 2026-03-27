0a76fc4f36ea5c936e2ee58703821e0c2705d76c
MdSaifu7 <mdsaifkhanahmad@gmail.com>
2026-03-27 12:59:41 +0530

    config axios for vercel

--- front-end/src/api/AxiosConfig.jsx
+++ front-end/src/api/AxiosConfig.jsx
@@ -1,15 +1,15 @@
+// import axios from "axios";
+// const instance = axios.create({
+//   baseURL: "http://localhost:3000/",
+// });
+// instance.defaults.withCredentials = true;
+// export default instance;
+
+//commented code for accesing the deployed backend server, uncomment the below code and comment the above code to access the deployed backend server
 import axios from "axios";
+
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://products-website-io.onrender.com",
 });
 instance.defaults.withCredentials = true;
 export default instance;
-
-//commented code for accesing the deployed backend server, uncomment the below code and comment the above code to access the deployed backend server
-// import axios from "axios";
-
-// const instance = axios.create({
-//   baseURL: "https://products-website-io.onrender.com",
-// });
-
-// export default instance;
